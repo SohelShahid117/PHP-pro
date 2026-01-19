e54b3bf73c0cd7643b0bb31e1cff9f3618ec53ab
SohelShahid117 <sohelshahid09@gmail.com>
2026-01-19 06:48:29 +0600

    How to Declare Variables in PHP done

---
+++ cls-1/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lec-1-5</title>
+    <style>
+        .hadi{
+            color:red;
+            
+        }
+    </style>
+    <?php
+    $name = "Sohel";
+    ?>
+</head>
+<body>
+    <?php 
+    echo "<h1 class='hadi' style='color:green'> Allaho Akbar </h1>";
+    ECHO "<h2>PHP is case sensitive but PHP keyword is not case sensitive</h2>";
+    echo $name;
+    //echo $Name; single line comment
+
+    #single line comment
+
+    /*
+    multi 
+    line
+    comment
+    */
+    ?>
+</body>
+</html>
---
+++ cls-1/variables.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name = "Adil";
+    ?>
+    <h1>hi</h1>
+    <?php echo "hello"?>;
+    <h2> <?php echo $name?> </h2>
+</body>
+</html>
