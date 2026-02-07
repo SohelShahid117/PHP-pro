5d80424e652e991112b65c37b4a4af431fe682e1
SohelShahid117 <sohelshahid09@gmail.com>
2026-02-08 00:04:03 +0600

    Superglobal Variables in PHP  done

--- cls-3/index.php
+++ cls-3/index.php
@@ -1,3 +1,13 @@
 <?php
-echo "hello cls-3";
+//Superglobal Variables in PHP 
+echo "hello cls-3 <br/>";
+echo "todays lec from Superglobal Variables in PHP <br/>";
+
+$cell = "01823012335";
+function getAddress(){
+    // global $cell;
+    // echo "address is : ".$cell;
+    echo "my address is : ".$GLOBALS['cell'];
+}
+getAddress();
 ?>
