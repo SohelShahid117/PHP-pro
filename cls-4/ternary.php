5fab0b32d5511c379c0c504dc0c8ba5bced4ae9b
SohelShahid117 <sohelshahid09@gmail.com>
2026-02-08 22:14:41 +0600

    php ternary operator done

---
+++ cls-4/ternary.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $age = 18;
+    $result = $age >=18?"Voter":"not voter";
+    echo $result;
+    ?>
+</body>
+</html>
