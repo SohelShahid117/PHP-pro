7aacd182d263694840b6115e7a2a2922a7cfd29a
SohelShahid117 <sohelshahid09@gmail.com>
2026-02-08 13:26:16 +0600

    string to integer done

---
+++ cls-4/typeCasting.php
@@ -0,0 +1,16 @@
+<?php
+
+$price = 20.77;
+$price2 = 990;
+// echo int($price)."<br/>";
+echo (int)$price."<br/>";
+echo var_dump((float)$price2)."<br/>";
+echo var_dump($price2)."<br/>";
+
+$cell = "01823012335";
+echo (int)$cell."<br/>";
+echo var_dump((int)$cell)."<br/>";
+echo var_dump(is_int((int)$cell))."<br/>";
+
+
+?>
