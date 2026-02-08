a40963d51ca92af615ab07a9f2753b94cc82bcbf
SohelShahid117 <sohelshahid09@gmail.com>
2026-02-08 11:33:47 +0600

    All PHP Data Types done

---
+++ cls-4/dataType.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+in php data types are 8 types:
+1.string
+2.integer
+3.float/double
+4.boolean
+5.array
+6.object
+7.null
+8.resource
+*/
+
+echo "hello data type <br/>";
+
+$person_name = 'Sohel Shahid';
+$person_cell = "01823012335";
+$person_address = "Chittagong";
+
+echo $person_name."<br/>";
+echo $person_cell."<br/>";
+
+?>
