85929671aeaf52b8345ff588e6cfdbc604e61667
SohelShahid117 <sohelshahid09@gmail.com>
2026-02-08 21:40:34 +0600

    php operator  done

---
+++ cls-4/operator.php
@@ -0,0 +1,19 @@
+<?php
+/*
+operator r :
+1.arithmetic operator
+2.assignment opeartor
+3.comparison operator
+4.increment/decrement operator
+5.logical operator
+6.string operator
+7.array operator
+
+*/
+
+$price = 10;
+$qty = 5;
+$total_price = $price*$qty;//operator,operant,expression
+echo $total_price;
+
+?>
