59833dff48ec26380a16a4bb2301996f36b4a986
SohelShahid117 <sohelshahid09@gmail.com>
2026-02-09 19:13:22 +0600

    PHP Do While Loop done

---
+++ cls-5/loop.php
@@ -0,0 +1,20 @@
+<?php
+/*
+while,
+do while,
+for,
+foreach*/
+
+$video = 1;
+while($video<=10){
+    echo "Video Number : $video <br/>";
+    $video++;
+}
+echo "next video <br/>";
+
+$start_point = 0;
+do{
+    echo "start point is $start_point <br/>";
+    $start_point++;
+}while($start_point<=10)
+?>
