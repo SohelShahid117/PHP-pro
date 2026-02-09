8f08d7df97ca8e6dca216063f766b27067bd8308
SohelShahid117 <sohelshahid09@gmail.com>
2026-02-09 19:27:07 +0600

    PHP For Loop  done

--- cls-5/loop.php
+++ cls-5/loop.php
@@ -16,5 +16,20 @@ $start_point = 0;
 do{
     echo "start point is $start_point <br/>";
     $start_point++;
-}while($start_point<=10)
+}while($start_point<=10);
+
+
+for($i=0;$i<=5;$i++){
+    echo "for loop : $i <br/>";
+}
+// exit();
+
+$info = array("Sohel","Shahid","Abdullah","Adil","Mariam");
+$len = count($info);
+
+echo $len."<br/>";
+for($i=0;$i<$len;$i++){
+    echo $info[$i]."<br/>";
+}
+
 ?>
