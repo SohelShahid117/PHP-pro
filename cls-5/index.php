dc371aeea26f0ec7444c912fe10b4a7e8be4b9da
SohelShahid117 <sohelshahid09@gmail.com>
2026-02-09 18:57:01 +0600

    PHP Switch Statement done

---
+++ cls-5/index.php
@@ -0,0 +1,31 @@
+<?php
+echo "hello <br/>";
+$current_day = date("D");
+echo $current_day."<br/>";
+
+switch($current_day){
+    case "Sat":
+        echo "Today is Saturday";
+        break;
+    case "Sun":
+        echo "Today is Sunday";
+        break;
+    case "Mon":
+        echo "Today is Monday";
+        break;
+    case "Tue":
+        echo "Today is Tuesday";
+        break;
+    case "Wed":
+        echo "Today is Wednesday";
+        break;
+    case "Thu":
+        echo "Today is Thursday";
+        break;
+    case "Fri":
+        echo "Today is Friday";
+        break;
+    default:
+        echo "Invalid day";
+}
+?>
