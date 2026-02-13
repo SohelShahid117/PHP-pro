7b1b49dfbb8128dce35ae4b01ab94ce2ae60fdff
SohelShahid117 <sohelshahid09@gmail.com>
2026-02-13 18:27:25 +0600

    15# array diff kye array fill built in function done

--- cls-8/array_dif.php
+++ cls-8/array_dif.php
@@ -8,10 +8,14 @@ $priceList=array(
 $priceList2=array(
     "a"=>50,
     "b"=>30,
-    "c"=>40
+    "cc"=>80
 );
 // $new_diff_arr=array_diff($priceList2,$priceList);
 $new_diff_arr=array_diff($priceList,$priceList2);
 // print_r($new_diff_arr);
 var_dump($new_diff_arr);
+echo "<br/>";
+
+$key_diff = array_diff_key($priceList,$priceList2);
+var_dump($key_diff);
 ?>
---
+++ cls-8/array_fill.php
@@ -0,0 +1,7 @@
+<?php
+// $create_arr_with_val = array_fill(1,4,"s");
+// $create_arr_with_val = array_fill(0,4,"s");
+$create_arr_with_val = array_fill(4,3,"s");
+// var_dump($create_arr_with_val);
+print_r($create_arr_with_val);
+?>
