58690bd8f16e38313a9b5a74908ddd9be3e71a5a
SohelShahid117 <sohelshahid09@gmail.com>
2026-02-16 16:00:21 +0600

    PHP SUPER Global SERVER Variables done

---
+++ cls-11/superglobal_server.php
@@ -0,0 +1,30 @@
+<?php
+ echo $_SERVER["SERVER_ADDR"];
+ echo "<br/>";
+echo $_SERVER["SERVER_NAME"];
+ echo "<br/>";
+echo $_SERVER["SERVER_SOFTWARE"];
+echo "<br/>";
+echo $_SERVER["SERVER_PORT"];
+echo "<br/>";
+echo $_SERVER["SERVER_ADMIN"];
+echo "<br/>";
+echo $_SERVER["SERVER_PROTOCOL"];
+echo "<br/>";
+echo $_SERVER["SERVER_SIGNATURE"];
+
+ echo "<br/>";
+ echo $_SERVER["REMOTE_ADDR"];
+ echo "<br/>";
+ echo $_SERVER["REMOTE_PORT"];
+ echo "<br/>";
+ echo $_SERVER["SCRIPT_NAME"];
+ echo "<br/>";
+ echo $_SERVER["SCRIPT_FILENAME"];
+ echo "<br/>";
+ echo $_SERVER["PHP_SELF"];
+ echo "<br/>";
+
+
+
+?>
