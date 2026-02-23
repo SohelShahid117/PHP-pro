63cd02b8c1f7f27f15b318d61023aa68b923ce6d
SohelShahid117 <sohelshahid09@gmail.com>
2026-02-23 22:01:43 +0600

    Part#05# JavaScript Operators done

--- cls-12-js/main.js
+++ cls-12-js/main.js
@@ -5,3 +5,16 @@ alert(ps);
 document.writeln("javascript mojar<br/>");
 document.writeln("<h1>javascript mojar<h1/>");
 document.writeln("<h1 style='color: red'>javascript mojar<h1/>");
+
+/*
+data types:
+1.number
+2.string
+3.boolean
+4.object
+5.array
+6.null
+7.undefined
+8.symbol
+9.bigint
+*/
