5bcca40c22b66d9b735d14f93f349e951b2e13c5
SohelShahid117 <sohelshahid09@gmail.com>
2026-03-06 12:51:30 +0600

    PHP Date Function done

---
+++ cls-13-php-form/date.php
@@ -0,0 +1,12 @@
+<?php
+echo "day : ". date("d")."<br>";
+echo "Day : ". date("D")."<br>";
+echo "month : ". date("m")."<br>";
+echo "Month : ". date("M")."<br>";
+echo "year : ". date("y")."<br>";
+echo "Year : ". date("Y")."<br>";
+echo "date : ". date("d/m/y")."<br>";
+echo "date : ". date("D-M-Y")."<br>";
+echo "&copy 2016-".date("Y");
+
+?>
